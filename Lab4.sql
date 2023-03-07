952d9be2371dcef35340c782bec63fb13ca78f6a
Alena Smolenchuk <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-03-07 14:45:26 +0300

    Update Lab4.sql

--- Lab4.sql
+++ Lab4.sql
@@ -1,33 +1,34 @@
---task 1
+
+--TASK 1
 
 CREATE TABLE deps( id integer primary key, name varchar(64) not null, region
 varchar(64) not null );
 
---task 2
+--TASK 2
 
 CREATE SEQUENCE deps_id_seq start with 1 cycle increment by 4 cache 1;
 
---task 3
+--TASK 3
 
 INSERT INTO deps(id, name, region) VALUES(nextval('deps_id_seq'),
 'Direction', 'Mars') returning * ;
 
--- task 4
+--TASK 4
 
 INSERT INTO deps(id, name, region) SELECT nextval('deps_id_seq'),
 department_name, substr(region_name,1,2) from departments join locations
 using(location_id) join countries using(country_id) join regions using(region_id)
 returning * ;
 
---task 5
+--TASK 5
 
 UPDATE deps SET region = 'Europe' WHERE name = 'Sales';
 
---task 6
+--TASK 6
 
 DELETE FROM deps WHERE (id%2)=0;
 
---task 8
+--TASK 8
 
 WITH recursive rmanager (employee_id,manager_id) AS ( SELECT employee_id,
 manager_id FROM employees WHERE employee_id = 111 UNION ALL
@@ -35,8 +36,9 @@ SELECT employees.employee_id, employees.manager_id FROM employees JOIN
 rmanager ON rmanager.manager_id = employees.employee_id ) SELECT *
 FROM rmanager;
 
---task 9
+--TASK 9
 DELETE FROM departments d WHERE location_id IN (SELECT
 location_id FROM locations WHERE country_id IN
 (SELECT country_id FROM countries
 WHERE char_length(country_id) = 2 ));
+
