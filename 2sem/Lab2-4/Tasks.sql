81cafff8f0d817790cca620ff8a6b91a3d1c3f47
Alena <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-04-23 18:40:13 +0300

    Update Tasks.sql

--- 2sem/Lab2-4/Tasks.sql
+++ 2sem/Lab2-4/Tasks.sql
@@ -21,4 +21,33 @@ SELECT * FROM count_product('Кот в сапогах', '2029-12-31');
 
 --task 2
 --task 3 
+
+CREATE OR REPLACE VIEW cl_view AS
+SELECT "Client".id_client, "Client".name_cl, "Client".bank_doc,
+       "Product".id_product
+FROM "Client" 
+     JOIN "Contract" ON "Client".id_client = "Contract".client_id
+	 LEFT JOIN "Product" ON "Contract".contract_id = "Product".id_contract
+GROUP BY "Product".id_product, "Client".id_client;
+	 
+SELECT * FROM cl_view;
+
+
+CREATE OR REPLACE FUNCTION update_client_description()
+RETURNS TRIGGER AS $$
+BEGIN
+  UPDATE "Client"
+  SET name_cl = NEW.name_cl,
+      bank_doc = NEW.bank_doc
+  WHERE id_client = NEW.id_client;
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER update_client_description_trigger
+INSTEAD OF INSERT ON cl_view
+FOR EACH ROW
+EXECUTE FUNCTION update_client_description();
+
 --task 4
